cf98054ecbb501514610e86e7d8bc01419a82795
Matthew Roberts <thew.roberts@gmail.com>
2022-11-03 23:46:24 -0400

    Added testing op mode to help test after replacing claw.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/ClawTestOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.dcs15815;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Claw Test", group = "Testing")
+public class ClawTestOpMode extends LinearOpMode
+{
+    ProductionBot bot;
+
+    @Override
+    public void runOpMode() {
+	   bot = new ProductionBot(hardwareMap, ProductionBotConfiguration.class, telemetry);
+
+	   waitForStart();
+
+	   while (!isStopRequested()) {
+
+		  if (gamepad1.a) {
+			 bot.claw.setClawPosition(0);
+		  } else if (gamepad1.b) {
+			 bot.claw.setClawPosition(1);
+		  }
+	   }
+
+    }
+}
