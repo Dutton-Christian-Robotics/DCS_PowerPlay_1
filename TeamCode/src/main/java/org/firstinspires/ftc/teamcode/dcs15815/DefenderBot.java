d00b2bda3a6509594a07244eae67c7009e7351b5
Matthew Roberts <thew.roberts@gmail.com>
2022-11-15 23:01:57 -0500

    Updated to public

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/DefenderBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/DefenderBot.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class DefenderBot {
 
     private HardwareMap hardwareMap;
-    protected DefenderBotConfiguration configuration;
+    public DefenderBotConfiguration configuration;
     public DefenderBotDrivetrain drivetrain;
     public DefenderBotSystem sensors;
     public DefenderBotSystem navigation;
