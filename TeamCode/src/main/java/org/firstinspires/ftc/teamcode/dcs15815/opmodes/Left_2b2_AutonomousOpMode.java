626b37efd8c954707d9040c714884b9b4b1fec11
Matthew Roberts <thew.roberts@gmail.com>
2022-12-09 18:54:37 -0500

    Autonomous opmodes as of the end of Friday 2022-12-09.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes/Left_2b2_AutonomousOpMode.java
@@ -0,0 +1,215 @@
+package org.firstinspires.ftc.teamcode.dcs15815.opmodes;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.dcs15815.EdwardScissorliftBot.ESBConfiguration;
+import org.firstinspires.ftc.teamcode.dcs15815.EdwardScissorliftBot.EdwardScissorliftAutonomousBot;
+/*
+
+ABANDONED? attempted spline and other compounbd methods to drive to high junction and the stack.
+	Was having a hard time getting placement to be as precise as desired.
+
+	Doesn't seem worth the change.
+
+ */
+
+@Disabled
+@Autonomous(name = "Left 2b2 Autonomous", group = "Left")
+public class Left_2b2_AutonomousOpMode extends LinearOpMode {
+    EdwardScissorliftAutonomousBot bot;
+    int foundTagId = -1;
+    double CLOCKWISE_45 = Math.toRadians(-45);
+    double COUNTER_CLOCKWISE_45 = Math.toRadians(45);
+    double CLOCKWISE_90 = Math.toRadians(-90);
+    double COUNTER_CLOCKWISE_90 = Math.toRadians(90);
+    double CLOCKWISE_135 = Math.toRadians(-135);
+    double COUNTER_CLOCKWISE_135 = Math.toRadians(135);
+
+    int LIFT_POSITION_HIGH = ESBConfiguration.LIFT_POSITION_HIGH;
+    int LIFT_POSITION_BELOW_HIGH = ESBConfiguration.LIFT_POSITION_BELOW_HIGH;
+    int LIFT_POSITION_STACKTOP = ESBConfiguration.LIFT_POSITION_STACKTOP;
+    int LIFT_POSITION_LOW = ESBConfiguration.LIFT_POSITION_LOW;
+    int LIFT_POSITION_GROUND = ESBConfiguration.LIFT_POSITION_GROUND;
+
+
+    @Override
+    public void runOpMode() {
+	   telemetry.addData("Position", "Left");
+	   telemetry.addData("Bot", "initializing...");
+
+	   bot = new EdwardScissorliftAutonomousBot(hardwareMap, ESBConfiguration.class, telemetry);
+
+	   telemetry.addData("claw", bot.claw != null);
+	   telemetry.update();
+
+
+//	   Pose2d startingPose = bot.configPose2d("NAVIGATION_START_RED_LEFT");
+	   Pose2d startingPose = new Pose2d();
+
+	   bot.claw.close();
+
+	   while (!isStarted() && !isStopRequested()) {
+		  int mostRecentlyFoundTagId = bot.vision.searchForTags();
+		  if (mostRecentlyFoundTagId > 0) {
+			 foundTagId = mostRecentlyFoundTagId;
+			 telemetry.addData("Signal Position Found", foundTagId);
+		  } else if (foundTagId == -1) {
+			 telemetry.addData("Signal", "searching...");
+		  }
+		  telemetry.update();
+		  sleep(20);
+	   }
+
+	   bot.lift.setPosition( LIFT_POSITION_LOW);
+	   // sleep(1000);
+
+	   // TODO: replace t1-t3 with spline?
+	   // Drive forward
+
+
+	   Trajectory t1_initialForward = bot.navigation.trajectoryBuilder(new Pose2d(0, 0, 0))
+		  .splineTo(new Vector2d(48, 0), 0)
+		  .addDisplacementMarker(() -> {
+			 bot.lift.setPosition( LIFT_POSITION_HIGH);
+		  })
+		  .splineTo(new Vector2d(61, -6), CLOCKWISE_45)
+		  .build();
+
+	   bot.navigation.followTrajectory(t1_initialForward);
+	   bot.claw.open(); sleep(250);
+
+
+	   Trajectory t2_backup = bot.navigation.trajectoryBuilder(t1_initialForward.end())
+		  .lineTo(new Vector2d(48, 2))
+		  .build();
+	   bot.navigation.followTrajectory(t2_backup);
+
+	   Trajectory t3_toStack = bot.navigation.trajectoryBuilder(t2_backup.end())
+		  .addDisplacementMarker(() -> {
+			 bot.lift.setPosition( LIFT_POSITION_STACKTOP);
+		  })
+		  .lineToLinearHeading(new Pose2d(38, 31, COUNTER_CLOCKWISE_90))
+		  .build();
+	   bot.navigation.followTrajectory(t3_toStack);
+
+
+
+//	   // Forward to the junction after turn
+//	   Trajectory t2_approachHighJunction = bot.navigation.trajectoryBuilder(t1_initialForward.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+//			 .forward(7)
+//			 .build();
+//
+//	   // back away from junction
+//	   Trajectory t3_backAwayFromHighJunction = bot.navigation.trajectoryBuilder(t2_approachHighJunction.end())
+//			 .back(7)
+//			 .build();
+//
+//	   // TODO: replace with spline?
+//	   // drive toward stack after turn
+//	    Trajectory t4_approachStack = bot.navigation.trajectoryBuilder(t3_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, COUNTER_CLOCKWISE_135)))
+//		  .forward(28)
+//		  .build();
+//
+//	    // TODO: use spline?
+//	   Trajectory t5_splineToHighJunction = bot.navigation.trajectoryBuilder(new Pose2d(0, 0, COUNTER_CLOCKWISE_90))
+//		  .lineToConstantHeading(new Vector2d(-2, -12))
+//		  .lineToSplineHeading(new Pose2d(4, -27, CLOCKWISE_45))
+//		  .build();
+
+
+//	   .lineToSplineHeading(new Pose2d(4, -27, CLOCKWISE_45))
+
+
+ //	   Trajectory t5_leaveStack = bot.navigation.trajectoryBuilder(t4_approachStack.end())
+//			 .back(24)
+//			 .build();
+
+	   // drive toward junction
+//	   Trajectory t6_approachHighJunction = bot.navigation.trajectoryBuilder(t5_leaveStack.end().plus(new Pose2d(0, 0, CLOCKWISE_135)))
+//			 .forward(9)
+//			 .build();
+//
+//	   // back away from junction
+//	   Trajectory t7_backAwayFromHighJunction = bot.navigation.trajectoryBuilder(t6_approachHighJunction.end())
+//			 .back(10)
+//			 .build();
+
+
+
+//	   bot.navigation.turn(CLOCKWISE_45);
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(750);
+//
+//	   bot.navigation.followTrajectory(t2_approachHighJunction);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_BELOW_HIGH); sleep(250);
+//	   bot.claw.open(); sleep(250);
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(250);
+//
+//	   bot.navigation.followTrajectory(t3_backAwayFromHighJunction);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_STACKTOP); //sleep(1000);
+//
+//	   bot.navigation.turn(COUNTER_CLOCKWISE_135);
+//
+//	   bot.navigation.followTrajectory(t4_approachStack);
+//
+//	   bot.claw.close(); sleep(750);
+//	   bot.lift.setPosition( LIFT_POSITION_LOW); sleep(500);
+//
+//	   bot.navigation.followTrajectory(t5_splineToHighJunction);
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(500);
+
+	   // ground pound
+//	   bot.lift.setPosition( LIFT_POSITION_GROUND); sleep(500);
+//
+//	   bot.navigation.turn(CLOCKWISE_135);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(500);
+//
+//	   bot.navigation.followTrajectory(t6_approachHighJunction);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_BELOW_HIGH); sleep(250);
+//	   bot.claw.open(); sleep(250);
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(500);
+//
+//	   bot.navigation.followTrajectory(t7_backAwayFromHighJunction);
+//	   bot.lift.setPosition( LIFT_POSITION_GROUND); sleep(250);
+//	   bot.navigation.turn(CLOCKWISE_45);
+
+	   if (foundTagId < 1) {
+		  // really should do something here
+
+	   } else if (foundTagId == 1) {
+//
+//		  Trajectory t8_backIntoZone1 = bot.navigation.trajectoryBuilder(t7_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+//				.back(25)
+//				.build();
+//		  Trajectory t9_forwardWithinZone1 = bot.navigation.trajectoryBuilder(t8_backIntoZone1.end().plus(new Pose2d(0, 0, CLOCKWISE_90)))
+//				.forward(21)
+//				.build();
+//
+//		  bot.navigation.followTrajectory(t8_backIntoZone1);
+//		  bot.navigation.turn(CLOCKWISE_90);
+//		  bot.navigation.followTrajectory(t9_forwardWithinZone1);
+
+	   } else if (foundTagId == 2) {
+//		  bot.navigation.turn(CLOCKWISE_90);
+
+	   } else if (foundTagId == 3) {
+//		  Trajectory t8_forwardIntoZone3 = bot.navigation.trajectoryBuilder(t7_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+//				.forward(26)
+//				.build();
+//		  Trajectory t9_forwardWithinZone3 = bot.navigation.trajectoryBuilder(t8_forwardIntoZone3.end().plus(new Pose2d(0, 0, CLOCKWISE_90)))
+//				.forward(23)
+//				.build();
+//		  bot.navigation.followTrajectory(t8_forwardIntoZone3);
+//		  bot.navigation.turn(CLOCKWISE_90);
+//		  bot.navigation.followTrajectory(t9_forwardWithinZone3);
+	   }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes/Left_2b3_AutonomousOpMode.java
@@ -0,0 +1,256 @@
+package org.firstinspires.ftc.teamcode.dcs15815.opmodes;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.dcs15815.EdwardScissorliftBot.ESBConfiguration;
+import org.firstinspires.ftc.teamcode.dcs15815.EdwardScissorliftBot.EdwardScissorliftAutonomousBot;
+/*
+
+Scores pre-loaded cone on high junction that heads to stack and places three stack cones
+on nearest low junction, doing so by staying closer to the stack and using a 113 degree angle.
+This wasn't proving as precise at dropping on the low junction as the next opmode.
+
+Also, scoring four cones used up almost all time. Could not get to park in correct location. Will
+try again once we install odometry pods.
+
+ */
+
+@Autonomous(name = "Left 2b3 Autonomous", group = "Left")
+public class Left_2b3_AutonomousOpMode extends LinearOpMode {
+    EdwardScissorliftAutonomousBot bot;
+    int foundTagId = -1;
+    double CLOCKWISE_45 = Math.toRadians(-45);
+    double COUNTER_CLOCKWISE_45 = Math.toRadians(45);
+
+    double CLOCKWISE_90 = Math.toRadians(-90);
+    double COUNTER_CLOCKWISE_90 = Math.toRadians(90);
+
+    double CLOCKWISE_113 = Math.toRadians(-113);
+    double COUNTER_CLOCKWISE_113 = Math.toRadians(113);
+
+    double CLOCKWISE_135 = Math.toRadians(-135);
+    double COUNTER_CLOCKWISE_135 = Math.toRadians(135);
+
+    int LIFT_POSITION_HIGH = ESBConfiguration.LIFT_POSITION_HIGH;
+    int LIFT_POSITION_BELOW_HIGH = ESBConfiguration.LIFT_POSITION_BELOW_HIGH;
+    int LIFT_POSITION_STACKTOP = ESBConfiguration.LIFT_POSITION_STACKTOP;
+    int LIFT_POSITION_LOW = ESBConfiguration.LIFT_POSITION_LOW;
+    int LIFT_POSITION_GROUND = ESBConfiguration.LIFT_POSITION_GROUND;
+
+
+    @Override
+    public void runOpMode() {
+	   telemetry.addData("Position", "Left");
+	   telemetry.addData("Bot", "initializing...");
+
+	   bot = new EdwardScissorliftAutonomousBot(hardwareMap, ESBConfiguration.class, telemetry);
+
+	   telemetry.addData("claw", bot.claw != null);
+	   telemetry.update();
+
+
+//	   Pose2d startingPose = bot.configPose2d("NAVIGATION_START_RED_LEFT");
+	   Pose2d startingPose = new Pose2d();
+
+	   Trajectory t1_initialForward = bot.navigation.trajectoryBuilder(startingPose)
+			 .forward(57)
+			 .build();
+	   Trajectory t2_approachHighJunction = bot.navigation.trajectoryBuilder(t1_initialForward.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+			 .forward(7)
+			 .build();
+	   Trajectory t3_backAwayFromHighJunction = bot.navigation.trajectoryBuilder(t2_approachHighJunction.end())
+			 .back(7)
+			 .build();
+	   Trajectory t4_approachStack = bot.navigation.trajectoryBuilder(t3_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, COUNTER_CLOCKWISE_135)))
+			 .forward(28)
+			 .build();
+	   Trajectory t5_leaveStack = bot.navigation.trajectoryBuilder(t4_approachStack.end())
+			 .back(6)
+			 .build();
+	   Trajectory t6_approachLowJunction = bot.navigation.trajectoryBuilder(t5_leaveStack.end().plus(new Pose2d(0, 0, COUNTER_CLOCKWISE_113)))
+			 .forward(6)
+			 .build();
+	   Trajectory t7_leaveLowJunction = bot.navigation.trajectoryBuilder(t6_approachLowJunction.end())
+			 .back(6)
+			 .build();
+	   Trajectory t8_approachStack = bot.navigation.trajectoryBuilder(t7_leaveLowJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_113)))
+			 .forward(7)
+			 .build();
+	   Trajectory t9_leaveStack = bot.navigation.trajectoryBuilder(t8_approachStack.end())
+			 .back(6)
+			 .build();
+	   Trajectory t10_approachLowJunction = bot.navigation.trajectoryBuilder(t9_leaveStack.end().plus(new Pose2d(0, 0, COUNTER_CLOCKWISE_113)))
+			 .forward(6)
+			 .build();
+	   Trajectory t11_leaveLowJunction = bot.navigation.trajectoryBuilder(t10_approachLowJunction.end())
+			 .back(6)
+			 .build();
+	   Trajectory t12_approachStack = bot.navigation.trajectoryBuilder(t11_leaveLowJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_113)))
+			 .forward(7)
+			 .build();
+	   Trajectory t13_leaveStack = bot.navigation.trajectoryBuilder(t12_approachStack.end())
+			 .back(6)
+			 .build();
+	   Trajectory t14_approachLowJunction = bot.navigation.trajectoryBuilder(t13_leaveStack.end().plus(new Pose2d(0, 0, COUNTER_CLOCKWISE_113)))
+			 .forward(6)
+			 .build();
+
+
+
+	   bot.claw.close();
+
+	   while (!isStarted() && !isStopRequested()) {
+		  int mostRecentlyFoundTagId = bot.vision.searchForTags();
+		  if (mostRecentlyFoundTagId > 0) {
+			 foundTagId = mostRecentlyFoundTagId;
+			 telemetry.addData("Signal Position Found", foundTagId);
+		  } else if (foundTagId == -1) {
+			 telemetry.addData("Signal", "searching...");
+		  }
+		  telemetry.update();
+		  sleep(20);
+	   }
+
+	   bot.lift.setPosition( LIFT_POSITION_LOW);
+	   // sleep(1000);
+
+	   // Drive forward
+	   bot.navigation.followTrajectory(t1_initialForward);
+	   bot.navigation.turn(CLOCKWISE_45);
+	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(750);
+
+
+	   // Forward to the junction after turn
+	   bot.navigation.followTrajectory(t2_approachHighJunction);
+	   bot.lift.setPosition( LIFT_POSITION_BELOW_HIGH); sleep(250);
+	   bot.claw.open(); sleep(250);
+	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(250);
+
+
+	   // back away from junction
+	   bot.navigation.followTrajectory(t3_backAwayFromHighJunction);
+	   bot.lift.setPosition( LIFT_POSITION_STACKTOP); //sleep(1000);
+	   bot.navigation.turn(COUNTER_CLOCKWISE_135);
+
+	   // drive toward stack after turn
+	   bot.navigation.followTrajectory(t4_approachStack);
+	   bot.claw.close(); sleep(250);
+	   bot.lift.setPosition( LIFT_POSITION_LOW);
+
+	    // drive away from stack
+	   bot.navigation.followTrajectory(t5_leaveStack);
+	   bot.navigation.turn(COUNTER_CLOCKWISE_113);
+
+	   bot.navigation.followTrajectory(t6_approachLowJunction);
+	   bot.lift.setPosition(LIFT_POSITION_GROUND);sleep(200);
+	   bot.claw.open();
+	   bot.lift.setPosition(LIFT_POSITION_LOW);
+
+	   bot.navigation.followTrajectory(t7_leaveLowJunction);
+	   bot.navigation.turn(CLOCKWISE_113);
+
+	   bot.lift.setPosition( LIFT_POSITION_STACKTOP);
+	   bot.navigation.followTrajectory(t8_approachStack);
+	   bot.claw.close(); sleep(250);
+	   bot.lift.setPosition( LIFT_POSITION_LOW);
+
+	   // drive away from stack
+	   bot.navigation.followTrajectory(t9_leaveStack);
+	   bot.navigation.turn(COUNTER_CLOCKWISE_113);
+
+	   bot.navigation.followTrajectory(t10_approachLowJunction);
+	   bot.lift.setPosition(LIFT_POSITION_GROUND);sleep(200);
+	   bot.claw.open();
+	   bot.lift.setPosition(LIFT_POSITION_LOW);
+
+	   bot.navigation.followTrajectory(t11_leaveLowJunction);
+	   bot.navigation.turn(CLOCKWISE_113);
+
+	   bot.lift.setPosition( LIFT_POSITION_STACKTOP); sleep(150);
+	   bot.navigation.followTrajectory(t12_approachStack);
+	   bot.claw.close(); sleep(250);
+	   bot.lift.setPosition( LIFT_POSITION_LOW);
+
+	   // drive away from stack
+	   bot.navigation.followTrajectory(t13_leaveStack);
+	   bot.navigation.turn(COUNTER_CLOCKWISE_113);
+
+	   bot.navigation.followTrajectory(t14_approachLowJunction);
+	   bot.lift.setPosition(LIFT_POSITION_GROUND);sleep(200);
+	   bot.claw.open();
+	   bot.lift.setPosition(LIFT_POSITION_LOW);
+
+
+
+//	   // drive toward junction
+//	   Trajectory t6_approachHighJunction = bot.navigation.trajectoryBuilder(t5_leaveStack.end().plus(new Pose2d(0, 0, CLOCKWISE_135)))
+//			 .forward(9)
+//			 .build();
+//
+//	   // back away from junction
+//	   Trajectory t7_backAwayFromHighJunction = bot.navigation.trajectoryBuilder(t6_approachHighJunction.end())
+//			 .back(10)
+//			 .build();
+
+
+
+
+
+
+
+
+
+
+
+//	   // ground pound
+//	   bot.lift.setPosition( LIFT_POSITION_GROUND); sleep(500);
+//
+//	   bot.navigation.turn(CLOCKWISE_135);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(500);
+//
+//	   bot.navigation.followTrajectory(t6_approachHighJunction);
+//
+//	   bot.lift.setPosition( LIFT_POSITION_BELOW_HIGH); sleep(250);
+//	   bot.claw.open(); sleep(250);
+//	   bot.lift.setPosition( LIFT_POSITION_HIGH); sleep(500);
+//
+//	   bot.navigation.followTrajectory(t7_backAwayFromHighJunction);
+//	   bot.lift.setPosition( LIFT_POSITION_GROUND); sleep(250);
+//	   bot.navigation.turn(CLOCKWISE_45);
+
+	   if (foundTagId < 1) {
+		  // really should do something here
+
+	   } else if (foundTagId == 1) {
+
+//		  Trajectory t8_backIntoZone1 = bot.navigation.trajectoryBuilder(t7_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+//				.back(25)
+//				.build();
+//		  Trajectory t9_forwardWithinZone1 = bot.navigation.trajectoryBuilder(t8_backIntoZone1.end().plus(new Pose2d(0, 0, CLOCKWISE_90)))
+//				.forward(21)
+//				.build();
+//
+//		  bot.navigation.followTrajectory(t8_backIntoZone1);
+//		  bot.navigation.turn(CLOCKWISE_90);
+//		  bot.navigation.followTrajectory(t9_forwardWithinZone1);
+
+	   } else if (foundTagId == 2) {
+//		  bot.navigation.turn(CLOCKWISE_90);
+
+	   } else if (foundTagId == 3) {
+//		  Trajectory t8_forwardIntoZone3 = bot.navigation.trajectoryBuilder(t7_backAwayFromHighJunction.end().plus(new Pose2d(0, 0, CLOCKWISE_45)))
+//				.forward(26)
+//				.build();
+//		  Trajectory t9_forwardWithinZone3 = bot.navigation.trajectoryBuilder(t8_forwardIntoZone3.end().plus(new Pose2d(0, 0, CLOCKWISE_90)))
+//				.forward(23)
+//				.build();
+//		  bot.navigation.followTrajectory(t8_forwardIntoZone3);
+//		  bot.navigation.turn(CLOCKWISE_90);
+//		  bot.navigation.followTrajectory(t9_forwardWithinZone3);
+	   }
+    }
+}
