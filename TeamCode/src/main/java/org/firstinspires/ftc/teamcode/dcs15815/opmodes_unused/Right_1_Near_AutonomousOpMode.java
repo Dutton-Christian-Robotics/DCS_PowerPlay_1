0c33e0b1b3556d743a0f8fe37f4f88d26cab954e
Matthew Roberts <thew.roberts@gmail.com>
2022-12-14 19:06:14 -0500

    Moving unused opmodes to separate package.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_unused/Right_1_Near_AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_unused/Right_1_Near_AutonomousOpMode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.dcs15815.opmodes;
+package org.firstinspires.ftc.teamcode.dcs15815.opmodes_unused;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -12,16 +12,15 @@ import org.firstinspires.ftc.teamcode.dcs15815.EdwardScissorliftBot.ESBConfigura
  Alliance Color: either
  Starting Position: touching wall, contained within tile F5 (red) or A2 (blue); camera pointed at signal
  Tasks:
- 	1) determine signal zone from team-supplied signal sleeve
- 	2) drive to angled position near midfield right tall junction
+	1) determine signal zone from team-supplied signal sleeve
+ 	2) drive to position near closest tall junction
  	3) deliver preloaded cone
  	4) park within signal zone (no fallback for not sensing signal)
- 	5) turn towards alliance
- */
+  */
 
 @Disabled
-@Autonomous(name = "Right 1c Autonomous", group = "Right")
-public class Right_1_Midfield_AutonomousOpMode extends LinearOpMode {
+@Autonomous(name = "Right 1: near junction", group = "Right")
+public class Right_1_Near_AutonomousOpMode extends LinearOpMode {
     EdwardScissorliftAutonomousBot bot;
     int foundTagId = -1;
 
@@ -53,56 +52,53 @@ public class Right_1_Midfield_AutonomousOpMode extends LinearOpMode {
 	   bot.lift.setPosition( ESBConfiguration.LIFT_POSITION_LOW); sleep(1500);
 
 	   Trajectory traj1 = bot.navigation.trajectoryBuilder(startingPose)
-			 .forward(55)
+			 .forward(30)
 			 .build();
 
-	   Trajectory traj2 = bot.navigation.trajectoryBuilder(traj1.end().plus(new Pose2d(0, 0, Math.toRadians(45))))
-			 .forward(10)
+	   Trajectory traj2 = bot.navigation.trajectoryBuilder(traj1.end().plus(new Pose2d(0, 0, Math.toRadians(90))))
+			 .forward(22)
 			 .build();
 
-	   Trajectory traj3 = bot.navigation.trajectoryBuilder(traj2.end())
+	   Trajectory traj3 = bot.navigation.trajectoryBuilder(traj2.end().plus(new Pose2d(0, 0, Math.toRadians(-45))))
+			 .forward(12)
+			 .build();
+
+	   Trajectory traj4 = bot.navigation.trajectoryBuilder(traj3.end())
 			 .back(11)
 			 .build();
 
-	   bot.navigation.followTrajectory(traj1);
-	   bot.navigation.turn(Math.toRadians(45));
-	   bot.lift.setPosition( ESBConfiguration.LIFT_POSITION_HIGH); sleep(1000);
-	   bot.navigation.followTrajectory(traj2);
-	   bot.claw.open(); sleep(2000);
-	   bot.navigation.followTrajectory(traj3);
-	   bot.lift.setPosition( ESBConfiguration.LIFT_POSITION_GROUND); sleep(2000);
-	   bot.navigation.turn(Math.toRadians(45));
+	   Trajectory traj5_1 = bot.navigation.trajectoryBuilder(traj4.end().plus(new Pose2d(0, 0, Math.toRadians(-45))))
+			 .forward(14)
+			 .build();
+	   Trajectory traj5_2 = bot.navigation.trajectoryBuilder(traj4.end().plus(new Pose2d(0, 0, Math.toRadians(-135))))
+			 .forward(23)
+			 .build();
+	   Trajectory traj5_3 = bot.navigation.trajectoryBuilder(traj4.end().plus(new Pose2d(0, 0, Math.toRadians(-135))))
+			 .forward(48)
+			 .build();
 
+	   bot.navigation.followTrajectory(traj1);
+	   bot.navigation.turn(Math.toRadians(90));
+	   bot.navigation.followTrajectory(traj2);
+	   bot.lift.setPosition( ESBConfiguration.LIFT_POSITION_HIGH); sleep(1250);
+	   bot.navigation.turn(Math.toRadians(-45));
+	   bot.navigation.followTrajectory(traj3);
+	   bot.claw.open(); sleep(2000);
+	   bot.navigation.followTrajectory(traj4);
+	   bot.lift.setPosition( ESBConfiguration.LIFT_POSITION_GROUND); sleep(2000);
 
 
 	   if (foundTagId < 1) {
 
 	   } else if (foundTagId == 1) {
-		  Trajectory traj4 = bot.navigation.trajectoryBuilder(traj3.end().plus(new Pose2d(0, 0, Math.toRadians(45))))
-				.forward(23)
-				.build();
-		  Trajectory traj5 = bot.navigation.trajectoryBuilder(traj4.end().plus(new Pose2d(0, 0, Math.toRadians(90))))
-				.forward(24)
-				.build();
-		  bot.navigation.followTrajectory(traj4);
-		  bot.navigation.turn(Math.toRadians(90));
-		  bot.navigation.followTrajectory(traj5);
-
+		  bot.navigation.turn(Math.toRadians(-45));
+		  bot.navigation.followTrajectory(traj5_1);
 	   } else if (foundTagId == 2) {
-		  bot.navigation.turn(Math.toRadians(90));
-
+		  bot.navigation.turn(Math.toRadians(-135));
+		  bot.navigation.followTrajectory(traj5_2);
 	   } else if (foundTagId == 3) {
-		  Trajectory traj4 = bot.navigation.trajectoryBuilder(traj3.end().plus(new Pose2d(0, 0, Math.toRadians(45))))
-				.back(25)
-				.build();
-		  Trajectory traj5 = bot.navigation.trajectoryBuilder(traj4.end().plus(new Pose2d(0, 0, Math.toRadians(90))))
-				.forward(19)
-				.build();
-
-		  bot.navigation.followTrajectory(traj4);
-		  bot.navigation.turn(Math.toRadians(90));
-		  bot.navigation.followTrajectory(traj5);
-
+		  bot.navigation.turn(Math.toRadians(-135));
+		  bot.navigation.followTrajectory(traj5_3);
 	   }
     }
 }
